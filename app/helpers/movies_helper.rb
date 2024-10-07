a92b8b9e8893342ff492d21b152cadc44b3afe47
maxsmith271346 <maxsmith271346@tamu.edu>
2024-10-06 23:35:16 -0500

    Add styling

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,14 @@
 module MoviesHelper
     def sort_movies(label, column)
         order = (column == params[:column] && params[:order] == 'asc') ? 'desc' : 'asc'
-        link_to(label, movies_path(column: column, order: order, class: "sortable"))
+        css_class = column == params[:column] ? "sorted-#{params[:order]}" : ""
+        puts "css_class: #{css_class}"
+        arrow = sort_arrow(column)
+        link_to("#{label} #{arrow}".html_safe, movies_path(column: column, order: order), class: css_class)
     end
+
+    def sort_arrow(column)
+        column == params[:column] ?
+            (params[:order] == 'asc' ? '▲' : '▼') : ''
+      end
 end
